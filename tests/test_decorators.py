9799d7e992c615574dfaeae3b8e05a121749699a
yuriisthebest <yurimaas@hotmail.com>
2019-02-28 12:10:50 +0100

    Created test cases for decorator: strict_types

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,10 +3,36 @@ from utils.decorators import strict_types
 
 
 class TestDecorators(TestCase):
-    def test_type_hints_raises(self):
-        self.fail()
+    def test_argument_raises(self):
+        """
+        Test that checks if type_hint decorated functions raises TypeErrors
+         when arguments given are not of the correct type
+        :return: Test Passed
+        """
+        with self.assertRaises(TypeError):
+            type_hint_test(1, 1, 1)
+            type_hint_test('a', 'a', 'a')
+
+    def test_return_raises(self):
+        """
+        Test that checks if type_hint decorated functions raises TypeErrors
+         when return statements are not of the correct type
+        :return: Test Passed
+        """
+        with self.assertRaises(TypeError):
+            type_hint_test(1, 'x', 'x')
+
+    def test_type_hints_not_raises(self):
+        """
+        Test that checks if type_hint decorated functions
+         behave the same when all type requirements are met
+        :return: Test Passed
+        """
+        x = 1
+        self.assertEqual(type_hint_test(x, 'x', x), x, msg="Type hint didn't raise an error.")
 
 
 @strict_types
-def type_hint_test(a: int, b: str, c):
-    pass
+def type_hint_test(a: int, b: str, c) -> int:
+    """ Decorated Test Function """
+    return c
