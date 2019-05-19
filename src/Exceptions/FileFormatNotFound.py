abd9045835cc27ca4897649c38fa88eaebb74543
yuriisthebest <yurimaas@hotmail.com>
2019-05-19 16:20:09 +0200

    Added NotMergeableError

--- src/Exceptions/FileFormatNotFound.py
+++
@@ -1,8 +0,0 @@
-class PyWashException(Exception):
-    # Base Exception class for the PyWash module
-    pass
-
-
-class FileFormatNotFound(PyWashException):
-    """ Raised when a file has an unknown or unparsable format """
-    pass
